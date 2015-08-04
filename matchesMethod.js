b8235f49fb69be031ee8de3fcf0a7f2a6f7de722
FabioVergani <fabio.vergani.1975@gmail.com>
2015-08-04 21:03:13 +0200

    Update matchesMethod.js

--- matchesMethod.js
+++ matchesMethod.js
@@ -1,3 +1,19 @@
+function capitalize(t,k){var s=t;return s[0].toUpperCase()+(s=s.slice(1),k===1?s.toLowerCase():s);}
+//
+var matchesMethod=(function(e,vendors){
+ var o=e.prototype,p='matches';
+ if(!(p in o)){
+  p+='Selector';
+  if(!(p in o)){
+	for(var a,b=o,s=capitalize(p),i=0,m=vendors,l=m.length;i<l;i++){a=m[i]+s;alert(a);if(a in b){p=a;break;};};
+	if(p.length===15){p='';};
+  };
+ };
+ return p;
+})(window.Element,['webkit','moz','ms','o']);
+//
+
+/*
 var obj=Element.prototype, prop='matches';
 if(!(prop in obj)){
  prop='matchesSelector';
@@ -11,4 +27,4 @@ if(!(prop in obj)){
 };
 console.log(prop);
 return prop;
-//
+*/
