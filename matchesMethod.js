17361e481988eb51de37e59707ee087805362fbd
FabioVergani <fabio.vergani.1975@gmail.com>
2015-08-05 13:28:35 +0200

    Update matchesMethod.js

--- matchesMethod.js
+++ matchesMethod.js
@@ -1,3 +1,4 @@
+//console.clear();
 function capitalize(t,k){var s=t;return s[0].toUpperCase()+(s=s.slice(1),k===1?s.toLowerCase():s);}
 //
 var matchesMethod=(function(e,vendors){
@@ -9,9 +10,11 @@ var matchesMethod=(function(e,vendors){
 	if(p.length===15){p='';};
   };
  };
- return p;
+ return Function('e,s','return e.'+p+'(s);');
 })(window.Element,['webkit','moz','ms','o']);
-//
+//console.log(document.body.matches("html > body"));
+//console.log(matchesMethod(document.body,"html > body"));
+
 
 /*
 var obj=Element.prototype, prop='matches';
